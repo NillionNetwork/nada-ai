5b312b7504af4018e30b3b7e496190a14ab9ae6d
Mathias Leys <mathias.leys@nillion.com>
2024-06-05 14:09:33 +0200

    Fix ReLU logic

--- nada_ai/nn/activations.py
+++ nada_ai/nn/activations.py
@@ -1,15 +1,17 @@
 """NN activations logic"""
 
+from typing import Union
 import nada_algebra as na
 from nada_ai.nn.module import Module
-from nada_dsl import Integer
+from nada_dsl import Integer, NadaType, SecretInteger, SecretBoolean, PublicBoolean
 
 
 class ReLU(Module):
     """ReLU layer implementation"""
 
     def forward(self, x: na.NadaArray) -> na.NadaArray:
-        """Forward pass.
+        """
+        Forward pass.
 
         Args:
             x (na.NadaArray): Input array.
@@ -17,17 +19,38 @@ class ReLU(Module):
         Returns:
             na.NadaArray: Module output.
         """
-        dtype = type(x.item(0))
-        if dtype in (na.Rational, na.SecretRational):
-            mask = x.applypyfunc(
-                lambda a: (a > na.Rational(0)).if_else(
-                    Integer(1 << a.log_scale), Integer(0)
-                )
-            )
-            mask = mask.applypyfunc(lambda a: na.SecretRational(value=a))
+        if x.dtype in (na.Rational, na.SecretRational):
+            mask = x.apply(self._rational_relu)
         else:
-            mask = x.applypyfunc(
-                lambda a: (a > Integer(0)).if_else(Integer(1), Integer(0))
-            )
-        result = x * mask
-        return result
+            mask = x.apply(self._relu)
+
+        return x * mask
+
+    @staticmethod
+    def _rational_relu(value: Union[na.Rational, na.SecretRational]) -> na.SecretRational:
+        """
+        Element-wise ReLU logic for rational values.
+
+        Args:
+            value (Union[na.Rational, na.SecretRational]): Input rational.
+
+        Returns:
+            na.SecretRational: ReLU output rational.
+        """
+        rational_zero = na.rational(0, is_scaled=True)  # No need to rescale zero
+        above_zero: Union[PublicBoolean, SecretBoolean] = value > rational_zero
+        return above_zero.if_else(na.rational(1), rational_zero)
+
+    @staticmethod
+    def _relu(value: NadaType) -> SecretInteger:
+        """
+        Element-wise ReLU logic for NadaType values.
+
+        Args:
+            value (NadaType): Input nada value.
+
+        Returns:
+            SecretInteger: Output nada value.
+        """
+        above_zero: Union[PublicBoolean, SecretBoolean] = value > Integer(0)
+        return above_zero.if_else(Integer(1), Integer(0))
