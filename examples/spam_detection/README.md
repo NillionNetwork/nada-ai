47854c78f2014afdb529523f1c11b3965934373a
José Cabrero-Holgueras <jose.cabrero@nillion.com>
2024-06-12 14:36:53 +0000

    feat: added spam_detection demo

---
+++ examples/spam_detection/README.md
@@ -0,0 +1,11 @@
+# Text classification
+**This folder was generated using `nada init`**
+
+## How to run this test:
+
+0. Make sure all the dependencies are installed.
+1. Run `cd examples/spam_detection` to changed directory.
+2. Then, we can create our local devnet with: `bootstrap-nillion-devnet`. This command produces a local environment file `.env` file in the specific directory. So make sure to execute it in the specific directory.
+2. Compile the project using `nada build`.
+3. Run through the model provider script.
+4. Run through the client script.
--- nada_ai/linear_model/linear_regression.py
+++ nada_ai/linear_model/linear_regression.py
@@ -34,3 +34,21 @@ class LinearRegression(Module):
         if self.intercept is None:
             return self.coef @ x
         return self.coef @ x + self.intercept
+
+
+class LogisticRegression(LinearRegression):
+    """Logistic regression implementation inheriting from LinearRegression"""
+
+    def __init__(
+        self, in_features: int, out_features: int, include_bias: bool = True
+    ) -> None:
+        """
+        Initialization.
+
+        Args:
+            in_features (int): Number of input features to regression.
+            num_classes (int): Number of classes to predict.
+            include_bias (bool, optional): Whether or not to include a bias term. Defaults to True.
+        """
+        self.coef = Parameter((out_features, in_features))
+        self.intercept = Parameter(out_features) if include_bias else None
